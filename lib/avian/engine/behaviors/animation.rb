e291d5372455cf09a2231bfcb5668d912b0f8baa
Daniel Inkpen <dan2552@gmail.com>
2019-10-12 17:06:26 +0100

    Fixes in Animation when using multiple animations for a single game object

--- lib/avian/engine/behaviors/animation.rb
+++ lib/avian/engine/behaviors/animation.rb
@@ -12,13 +12,45 @@ class Animation < Behavior
     reset
   end
 
+  # This can be used to conditionally animate. Can be used for example like:
+  #
+  # ```
+  # walking_animation.when(walking?)
+  # ```
+  #
+  # When the condition is true, the sprite will animate through the animation's
+  # sprite. When false, the animation object will reset itself so that the next
+  # time the animation is started, it'll be from the first animation frame
+  # again.
+  #
+  # When a game object has multiple animations, only one animation should be
+  # applied to the game object at once. Here's an example for a simple
+  # top-down sprite:
+  #
+  # ```
+  # walking_up = direction.y > 0
+  # walking_down = direction.y < 0
+  #
+  # walking_x_more_than_y = direction.x.abs > direction.y.abs
+  #
+  # walking_left = direction.x < 0 && walking_x_more_than_y
+  # walking_right = direction.x > 0 && walking_x_more_than_y
+  #
+  # walk_up_animation.when(walking_up && !walking_x_more_than_y)
+  # walk_down_animation.when(walking_down && !walking_x_more_than_y)
+  # walk_left_animation.when(walking_left && walking_x_more_than_y)
+  # walk_right_animation.when(walking_right && walking_x_more_than_y)
+  # ```
+  #
   def when(condition)
     if condition
       update
       game_object.sprite_name = key_frame
+      self.was_animating = true
     else
       reset
-      game_object.sprite_name = default_texture
+      game_object.sprite_name = default_texture if was_animating
+      self.was_animating = false
     end
   end
 
@@ -30,6 +62,7 @@ class Animation < Behavior
   attr_reader :texture_names
   attr_accessor :key_frame_index
   attr_accessor :time
+  attr_accessor :was_animating
 
   def update_key_frame
     while time > time_per_frame
