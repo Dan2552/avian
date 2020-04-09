a483d71cbe15b7440cbeac198976cb76c4d9a2b4
Daniel Inkpen <dan2552@gmail.com>
2020-04-09 20:13:39 +0100

    Fix bug where Collision comparison doesn't use rectangle like it should

--- lib/avian/engine/collision/comparison.rb
+++ lib/avian/engine/collision/comparison.rb
@@ -4,7 +4,7 @@ module Collision
       -> (potential_position) do
         potential_rectangle = Rectangle.new(potential_position, size)
         Collision.nearest_objects_to(potential_rectangle).none? do |nearby|
-          nearby.frame.intersects?(potential_position)
+          nearby.frame.intersects?(potential_rectangle)
         end
       end
     end
