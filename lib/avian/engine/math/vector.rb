489542227f0b1a07d2c1238fba991168d99080d0
Daniel Inkpen <dan2552@gmail.com>
2020-04-16 23:19:15 +0100

    Math::Vector comment

--- lib/avian/engine/math/vector.rb
+++ lib/avian/engine/math/vector.rb
@@ -1,5 +1,11 @@
 module Math
   module Vector
+    # - parameter current_position: The originating position Vector.
+    # - parameter target_position: The destination postiion Vector.
+    # - parameter maximum_step: If you were to call this method once per frame,
+    #   this would be the maximum amount you'd want the movement to be in that
+    #   single frame. E.g. could be the speed. Consider `speed * Time.delta`.
+    #
     def self.move_towards(current_position, target_position, maximum_step)
       vector = target_position - current_position
 
