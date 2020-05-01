16efa09789cb134c3261d21dda92b733096e1c4b
Daniel Inkpen <dan2552@gmail.com>
2020-05-01 20:46:08 +0100

    Theta*: Hopefully fix 1,1, 2,1 line of sight checks

--- lib/avian/engine/algorithms/theta_star/pathfinder.rb
+++ lib/avian/engine/algorithms/theta_star/pathfinder.rb
@@ -27,11 +27,72 @@ module ThetaStar
         vertex = graph[x, y]
         return false if vertex.nil?
 
-        rel_x = (x - lhs.x).abs
-        rel_y = (y - lhs.y).abs
-        return false if rel_x == 2 && rel_y == 1
-        return false if rel_x == 1 && rel_y == 2
-        return false if rel_x == 1 && rel_y == 1
+        rel_x = (x - lhs.x)
+        rel_y = (y - lhs.y)
+
+        if rel_x == 2 && rel_y == 1
+          check = graph[lhs.x + 1, lhs.y + 1]
+
+          return false if check.nil? || check.blocked?
+        end
+
+        if rel_x == -2 && rel_y == 1
+          check = graph[lhs.x - 1, lhs.y + 1]
+
+          return false if check.nil? || check.blocked?
+        end
+
+        if rel_x == 1 && rel_y == 2
+          check = graph[lhs.x + 1, lhs.y + 1]
+
+          return false if check.nil? || check.blocked?
+        end
+
+        if rel_x == 1 && rel_y == -2
+          check = graph[lhs.x + 1, lhs.y - 1]
+
+          return false if check.nil? || check.blocked?
+        end
+
+        if rel_x == 1 && rel_y == 1
+          check1 = graph[lhs.x, lhs.y + 1]
+
+          return false if check1.nil? || check1.blocked?
+
+          check2 = graph[lhs.x + 1, lhs.y]
+
+          return false if check2.nil? || check2.blocked?
+        end
+
+        if rel_x == -1 && rel_y == -1
+          check1 = graph[lhs.x, lhs.y - 1]
+
+          return false if check1.nil? || check1.blocked?
+
+          check2 = graph[lhs.x - 1, lhs.y]
+
+          return false if check2.nil? || check2.blocked?
+        end
+
+        if rel_x == 1 && rel_y == -1
+          check1 = graph[lhs.x, lhs.y - 1]
+
+          return false if check1.nil? || check1.blocked?
+
+          check2 = graph[lhs.x + 1, lhs.y]
+
+          return false if check2.nil? || check2.blocked?
+        end
+
+        if rel_x == -1 && rel_y == 1
+          check1 = graph[lhs.x - 1, lhs.y]
+
+          return false if check1.nil? || check1.blocked?
+
+          check2 = graph[lhs.x, lhs.y + 1]
+
+          return false if check2.nil? || check2.blocked?
+        end
 
         return false if vertex.neighbors.many? { |neighbor| neighbor.nil? || neighbor.blocked? }
 
