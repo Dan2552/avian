e81c2210a889078c4684d1e199ed49958702609b
Daniel Inkpen <dan2552@gmail.com>
2020-05-18 21:47:36 +0100

    Delete ios-gosu

--- lib/avian/platforms/ios-gosu/ios/app_delegate.rb
+++
@@ -1,9 +0,0 @@
-class AppDelegate
-  def application(application, didFinishLaunchingWithOptions:launchOptions)
-    UIApplication.sharedApplication.setIdleTimerDisabled(true)
-    size = UIScreen.mainScreen.bounds.size
-    @window = Avian::IOSGosuPlatform::Window.new(size.width, size.height)
-    @window.uikit_window.makeKeyAndVisible
-    true
-  end
-end
