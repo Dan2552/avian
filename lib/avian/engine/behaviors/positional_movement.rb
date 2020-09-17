2fe25e2cc2a37317cd796d3db921e3653af0dbf4
Daniel Inkpen <dan2552@gmail.com>
2020-09-17 16:45:40 +0100

    Update positional_movement.rb

--- lib/avian/engine/behaviors/positional_movement.rb
+++ lib/avian/engine/behaviors/positional_movement.rb
@@ -1,12 +1,14 @@
 # Moves a game object at it's movement speed towards a given position.
 #
 class PositionalMovement < Behavior
-  def initialize(game_object)
+  def initialize(game_object, speed = nil)
     @game_object = game_object
 
     require_respond_to(game_object, :position)
     require_respond_to(game_object, :position=)
     require_respond_to(game_object, :movement_speed)
+
+    @speed = speed || @game_object.movement_speed
   end
 
   # Move the game object towards the given position.
@@ -14,26 +16,23 @@ class PositionalMovement < Behavior
   # - returns: (boolean) true if the target has been reached.
   #
   def towards(target_position)
-    vector = target_position - game_object.position
+    return true if game_object.position == target_position
+
+    vector = Math::Vector.move_towards(
+      game_object.position,
+      target_position,
+      speed
+    )
 
     return true if vector.zero?
 
-    velocity = vector.normalize * game_object.movement_speed * Time.delta
-    potential_position = game_object.position + velocity
+    game_object.position += vector
 
-    potential_distance = Math::Distance.quick_distance(game_object.position, potential_position)
-    distance_to_target = Math::Distance.quick_distance(game_object.position, target_position)
-
-    if potential_distance < distance_to_target
-      game_object.position = potential_position
-      return false
-    else
-      game_object.position = target_position
-      return true
-    end
+    false
   end
 
   private
 
   attr_accessor :game_object
+  attr_reader :speed
 end
