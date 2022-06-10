2813de4f3c3ed993ecddb6e308c0987439ec5e94
Daniel Inkpen <dan2552@gmail.com>
2022-06-10 18:45:59 +0100

    Fix desktop builds

--- lib/avian/platforms/desktop-mruby/build_steps/sdl.rb
+++ lib/avian/platforms/desktop-mruby/build_steps/sdl.rb
@@ -7,8 +7,11 @@ unless File.directory?(File.join(build_dir, "desktop", "sdl"))
   system("git clone https://github.com/libsdl-org/SDL sdl") ||
     error("Failed to clone SDL")
 
-  log "\n--- Compiling SDL ---"
   FileUtils.cd(File.join(build_dir, "desktop", "sdl"))
+
+  system("git reset --hard 105f12061") || error("Failed to reset to known commit for sdl")
+
+  log "\n--- Compiling SDL ---"
   system("./configure --prefix=#{sdl_install} && make -j4 && make install") || begin
     FileUtils.remove_dir(File.join(build_dir, "desktop", "sdl"))
     error("Failed to compile SDL")
