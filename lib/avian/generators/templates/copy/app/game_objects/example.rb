a3ba8a0081637368ed76031eb3b5b111a582fe94
Daniel Inkpen <dan2552@gmail.com>
2022-11-08 13:42:53 +0000

    Give the example a sprite

--- lib/avian/generators/templates/copy/app/game_objects/example.rb
+++ lib/avian/generators/templates/copy/app/game_objects/example.rb
@@ -1,9 +1,21 @@
 class Example < GameObject::Base
+  renderable
+  string :sprite_name, default: "birds"
+  number :speed, default: -> { [500, 400, 300].sample }
+
   protected
 
   # Defines actions that should be performed in the game loop.
   #
   def update
     puts "hello world | #{Time.delta}"
+
+    if position.x > 300
+      self.speed = -500
+    elsif position.x < -300
+      self.speed = 500
+    end
+
+    self.position = Vector.new(position.x + speed * Time.delta, 0)
   end
 end
